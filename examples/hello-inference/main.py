d0eb7438d78d293a6965b35a4a9f946e1e209b95
xianyun <xianyun.tang@ailabs.tw>
2023-11-04 10:33:24 +0800

    demo infer2

--- examples/hello-inference/main.py
+++ examples/hello-inference/main.py
@@ -2,11 +2,13 @@ import os
 
 from flavor.serve.apps import InferAPP
 from flavor.serve.strategies import AiCOCOInputStrategy
+import time
 
 
-def infer(**kwargs):
-    return kwargs.get("images")
+def infer(images):
+    return images
 
 
 app = InferAPP(infer_function=infer, input_strategy=AiCOCOInputStrategy, output_strategy=None)
-app.run(port=int(os.getenv("PORT", 9000)))
+
+app.run()
--- flavor/serve/invocations/infer_invocation.py
+++ flavor/serve/invocations/infer_invocation.py
@@ -1,21 +1,19 @@
-import tempfile
 from typing import Any, Callable, Optional, Type
 
-import aiofile
-import starlette
 from fastapi import Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 from .base_invocation import BaseInvocationAPP
+from ..middlewares import TransformFileToFilenameMiddleware
 
 
 class InferInvocationAPP(BaseInvocationAPP):
     def __init__(
-        self,
-        infer_function: Callable,
-        input_strategy: Optional[Type[Any]] = None,
-        output_strategy: Optional[Type[Any]] = None,
+            self,
+            infer_function: Callable,
+            input_strategy: Optional[Type[Any]] = None,
+            output_strategy: Optional[Type[Any]] = None,
     ):
 
         super().__init__()
@@ -25,49 +23,28 @@ class InferInvocationAPP(BaseInvocationAPP):
             self.invocations,
             methods=["post"],
         )
+        self.app.add_middleware(TransformFileToFilenameMiddleware)
 
         self.infer_function = infer_function
         self.input_strategy = input_strategy() if input_strategy else None
         self.output_strategy = output_strategy() if output_strategy else None
 
     async def invocations(self, request: Request):
+        body = request.state.transformed_json
+        try:
+            if self.input_strategy:
+                kwargs = {**await self.input_strategy.apply(body)}
+            else:
+                kwargs = {**body}
 
-        form_data = await request.form()
+            result = self.infer_function(**kwargs)
 
-        with tempfile.TemporaryDirectory() as tempdir:
+            if self.output_strategy:
+                response = await self.output_strategy.apply(result)
+            else:
+                response = result
 
-            data = {}
-
-            for k in form_data:
-
-                if isinstance(form_data[k], starlette.datastructures.UploadFile):
-
-                    filenames = []
-                    for file_ in form_data.getlist(k):
-                        temp_file = tempfile.NamedTemporaryFile(
-                            delete=False, dir=tempdir, suffix=f"{file_.filename}"
-                        )
-                        async with aiofile.async_open(temp_file.name, "wb") as f:
-                            await f.write(await file_.read())
-                        filenames.append(temp_file.name)
-                    data[k] = filenames
-                else:
-                    data[k] = form_data[k]
-
-            try:
-                if self.input_strategy:
-                    kwargs = await self.input_strategy.apply(data)
-                else:
-                    kwargs = data
-
-                result = self.infer_function(**kwargs)
-
-                if self.output_strategy:
-                    response = await self.output_strategy.apply(**result)
-                else:
-                    response = result
-
-            except Exception as e:
-                response = {"error": str(e)}
+        except Exception as e:
+            response = {"error": str(e)}
 
         return JSONResponse(content=jsonable_encoder(response))
