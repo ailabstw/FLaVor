e8b32b51c1e3addf52b28ae719c004ca55c6a4d4
calvin89029 <calvin89029@ailabs.tw>
2024-02-21 23:02:31 +0800

    bugfix: xgb global model (#3)

--- flavor/gadget/xgboost.py
+++ flavor/gadget/xgboost.py
@@ -1,45 +1,21 @@
 import json
-import os
-import uuid
 
 
-def load_update_xgbmodel(bst, updateModelPath):
+def load_global_xgbmodel(bst, globalModelPath):
 
-    with open(updateModelPath, "r") as json_file:
-        updateModel = json.load(json_file)
-
-    prevModel = json.loads(bst.save_raw("json").decode())
-
-    update_num_trees = int(
-        updateModel["learner"]["gradient_booster"]["model"]["gbtree_model_param"]["num_trees"]
-    )
-    prev_num_trees = int(
-        prevModel["learner"]["gradient_booster"]["model"]["gbtree_model_param"]["num_trees"]
-    )
-
-    if prev_num_trees <= update_num_trees:
-        bst.load_model(updateModelPath)
-    else:
-        prevModel["learner"]["gradient_booster"]["model"]["tree_info"][
-            prev_num_trees - update_num_trees :
-        ] = updateModel["learner"]["gradient_booster"]["model"]["tree_info"]
-        update_trees = updateModel["learner"]["gradient_booster"]["model"]["trees"]
-        for tree_idx in range(update_num_trees):
-            update_trees[tree_idx]["id"] = prev_num_trees - update_num_trees + tree_idx
-            prevModel["learner"]["gradient_booster"]["model"]["trees"][
-                prev_num_trees - update_num_trees + tree_idx
-            ] = update_trees[tree_idx]
-
-        bst.load_model(bytearray(json.dumps(prevModel), "utf-8"))
+    bst.load_model(globalModelPath)
 
 
-def save_update_xgbmodel(bst, saveModelPath):
-    # if not saveModelPath.lower().endswith(".json"):
-    #    raise ValueError("Save path should be a json file. Got {}.".format(saveModelPath))
+def save_local_xgbmodel(bst, localModelPath):
 
-    _save_path = os.path.join(os.path.dirname(saveModelPath), str(uuid.uuid4()) + ".json")
+    ckpt = {}
+    ckpt["complete"] = json.loads(bst.save_raw("json").decode())
     if bst.num_boosted_rounds() == 1:
-        bst.save_model(_save_path)
+        ckpt["update"] = json.loads(bst.save_raw("json").decode())
     else:
-        bst[bst.num_boosted_rounds() - 1 : bst.num_boosted_rounds()].save_model(_save_path)
-    os.rename(_save_path, saveModelPath)
+        ckpt["update"] = json.loads(
+            bst[bst.num_boosted_rounds() - 1 : bst.num_boosted_rounds()].save_raw("json").decode()
+        )
+
+    with open(localModelPath, "w") as outfile:
+        json.dump(ckpt, outfile)
