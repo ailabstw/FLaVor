3ac6325c051b3339105c66db0a4a073d27e9f17e
ding3820 <ding3820@gmail.com>
2024-10-16 14:08:41 +0800

    feat: update tabular integration test

--- tests/integration_tests/inference/test_tabular_example.py
+++ tests/integration_tests/inference/test_tabular_example.py
@@ -2,55 +2,78 @@ import json
 import shutil
 from pathlib import Path
 
+import numpy as np
 import pytest
+import torch
+import yaml
 from httpx import ASGITransport, AsyncClient
 
-
-@pytest.mark.asyncio
-async def test_cls():
-    cls_example = pytest.importorskip("test_tasks.tabular_cls_example")
-    cls_app = cls_example.app
-
-    files = []
-    filepath = "examples/inference/test_data/tabular/cls/test_cls.csv"
-    filepath = Path(filepath)
-    file = open(filepath, "rb")
-    files.append(("files", (f"{filepath.name}", file)))
-
-    with open("examples/inference/test_data/tabular/cls/input.json", "r") as f:
-        data = json.load(f)
-
-    for k in data:
-        data[k] = json.dumps(data[k])
-
-    async with AsyncClient(
-        transport=ASGITransport(app=cls_app.app), base_url="http://test"
-    ) as client:
-        response = await client.post("/invocations", data=data, files=files)
-        assert response.status_code == 200
+from flavor.utils.infer_utils import compare_responses
 
 
-@pytest.mark.asyncio
-async def test_reg():
-    reg_example = pytest.importorskip("test_tasks.tabular_reg_example")
-    reg_app = reg_example.app
+@pytest.fixture
+def configs():
+    with open("tests/integration_tests/inference/test_configs.yaml", "r") as f:
+        test_configs = yaml.load(f, Loader=yaml.SafeLoader)
+    return test_configs["test_inference"]
 
-    files = []
-    filepath = "examples/inference/test_data/tabular/reg/test_reg.csv"
-    filepath = Path(filepath)
-    file = open(filepath, "rb")
-    files.append(("files", (f"{filepath.name}", file)))
 
-    with open("examples/inference/test_data/tabular/reg/input.json", "r") as f:
-        data = json.load(f)
+class TestTabularInference:
+    @pytest.fixture(autouse=True)
+    def _set_configs(self, configs):
+        self.configs = configs["test_tabular_inference"]
 
-    for k in data:
-        data[k] = json.dumps(data[k])
+        self.target_fields = {
+            "tables": (["id"], ["file_name"]),
+            "records": (
+                ["id"],
+                ["table_id", "row_indexes"],
+            ),
+            "categories": (["id"], ["name"]),
+            "regressions": (["id"], ["name"]),
+        }
+        torch.manual_seed(1999)
+        np.random.seed(1999)
 
-    async with AsyncClient(
-        transport=ASGITransport(app=reg_app.app), base_url="http://test"
-    ) as client:
-        response = await client.post("/invocations", data=data, files=files)
-        assert response.status_code == 200
+    def get_files_data(self, files_path, data_path):
+        files = []
+        files_path = Path(files_path)
+        file = open(files_path, "rb")
+        files.append(("files", (f"{files_path.name}", file)))
 
-    shutil.rmtree(reg_example.MODEL_PATH, ignore_errors=True)
+        with open(data_path, "r") as f:
+            data = json.load(f)
+        for k in data:
+            data[k] = json.dumps(data[k])
+
+        return files, data
+
+    async def run_test(self, name: str, files_path: str, data_path: str):
+        example = pytest.importorskip(f"test_tasks.{name}")
+        example_app = example.app
+
+        files, data = self.get_files_data(files_path, data_path)
+
+        async with AsyncClient(
+            transport=ASGITransport(app=example_app.app), base_url="http://test"
+        ) as client:
+            response = await client.post("/invocations", data=data, files=files)
+            assert response.status_code == 200
+            response_json = json.loads(response.text)
+
+        with open(f"tests/integration_tests/inference/data/{name}.json", "r") as f:
+            target = json.loads(f.read())
+
+        identical = compare_responses(response_json, target, self.target_fields)
+        assert identical
+
+        if name == "tabular_reg_example":
+            shutil.rmtree(example_app.MODEL_PATH, ignore_errors=True)
+
+    @pytest.mark.asyncio
+    async def test_tabular_cls(self):
+        await self.run_test(name="tabular_cls_example", **self.configs["tabular_cls_example"])
+
+    @pytest.mark.asyncio
+    async def test_tabular_reg(self):
+        await self.run_test(name="tabular_reg_example", **self.configs["tabular_reg_example"])
